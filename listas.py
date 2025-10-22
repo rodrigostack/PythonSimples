ad98688ba9a934a54868cd0b200b6c020cf4c73d
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2025-10-22 15:44:38 -0300

    Trabalhando com listas

---
+++ listas.py
@@ -0,0 +1,10 @@
+Fruits = ["Laranja", "Mamão", "Melão", "Abacaxi", "Uva", "Manga"]
+
+
+Fruits.append("Morango")
+
+print(Fruits)
+
+print(len(Fruits))
+
+print("Minha fruta favorita é ", Fruits[3])
