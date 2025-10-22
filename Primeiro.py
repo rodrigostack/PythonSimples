24e4d0eda1694ad923c356eaee80fa77da6e0320
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2025-10-22 14:24:09 -0300

    Primeiro programa input

---
+++ Primeiro.py
@@ -0,0 +1,12 @@
+# trabalhando com input
+
+name = input("Digite o seu nome:\n")
+estadoCivil = input("Qual seu estado civil?\n")
+idade = int(input("Qual a sua idade?\n"))
+altura = float(input("Qual a sua altura\n"))
+
+print(type(name))
+print(type(estadoCivil))
+print(type(idade))
+print(type(altura))
+
