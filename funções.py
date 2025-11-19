b95a0d4e6743c95754da424d8352d09667661e61
Rodrigo Oliveira <rodrigofrontback@gmail.com>
2025-11-19 11:11:05 -0300

    Começando com funções em Python

---
+++ funções.py
@@ -0,0 +1,7 @@
+def areatriangulo():
+    base = float(input("Digite o valor da base:\n"))
+    altura = float(input("Digite o valor da altura:\n"))
+    area = (base * altura)/2
+    print(area)
+
+areatriangulo()
